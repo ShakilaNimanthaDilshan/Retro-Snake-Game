fa14b6272fe9cf8231e341553702b7ef6108a61b
ShakilaNimanthaDilshan <ict22063@std.uwu.ac.lk>
2025-11-10 16:25:45 +0530

    Commit

---
+++ snake game/snake game/Cord.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace snake_game
+{
+    internal class Cord
+    {
+        private int x;
+        private int y;
+
+        public int X { get{ return x; } }
+        public int Y { get{ return y; } }
+
+        public Cord(int x, int y)
+        {
+            this.x = x;
+            this.y = y;
+        }
+
+        public override bool Equals(object? obj)
+        {
+            if ((obj == null) || !GetType().Equals(obj.GetType()))
+                return false;
+
+            Cord other = (Cord)obj;
+            return x == other.x && y == other.y;
+        }
+
+        public void ApplyMovementDirection(Direction direction)
+        {
+            switch(direction)
+            {
+                case Direction.Left:
+                    x--;
+                    break;
+                case Direction.Right:
+                    x++;
+                    break;
+                case Direction.Up:
+                    y--;
+                    break;
+                case Direction.Down:
+                    y++;
+                    break;
+            }
+        }
+    }
+}
---
+++ snake game/snake game/Program.cs
@@ -0,0 +1,33 @@
+﻿using snake_game;
+
+Cord gridDimensions = new Cord(50, 20);
+Cord snakePos = new Cord(10, 1);
+Random rand = new Random();
+Cord applePos = new Cord(rand.Next(1, gridDimensions.X-1), rand.Next(1, gridDimensions.Y-1));
+int frameDelaymilli = 100;
+Direction movementDirection = Direction.Down;
+
+while (true)
+{
+    Console.Clear();
+    snakePos.ApplyMovementDirection(movementDirection);
+
+    for (int y = 0; y < gridDimensions.Y; y++)
+    {
+        for (int x = 0; x < gridDimensions.X; x++)
+        {
+            Cord currentCord = new Cord(x, y);
+
+            if (snakePos.Equals(currentCord))
+                Console.Write("■");
+            else if (applePos.Equals(currentCord))
+                Console.Write("a");
+            else if (x == 0 || y == 0 || x == gridDimensions.X - 1 || y == gridDimensions.Y - 1)
+                Console.Write("#");
+            else
+                Console.Write(" ");
+        }
+        Console.WriteLine();
+    }
+    Thread.Sleep(frameDelaymilli);
+}
